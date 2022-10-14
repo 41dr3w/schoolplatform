269385c79ffb564607ab78d10c1504b3d6c7c3d0
41dr3w <aavlqz@gmail.com>
2022-10-14 18:52:23 -0300

    cleaning comments and other stuffs

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,13 +3,9 @@ const {User} = require("../models/user")
 const bcrypt = require("bcryptjs")
 const {validationResult} = require("express-validator")
 const { default: mongoose } = require("mongoose")
-//const generateToken = require("../helpers/generateJWT")
-
 
 
 //vistas
-
-
 
 //post C-reate
 const crearItem = async (req,res) => {
@@ -45,72 +41,6 @@ const crearSession = async (req,res) =>{
         res.status(501).json({error})
     }   
 }
-const loginUsuario = async (req, res) =>{
-    try {
-        const err = validationResult(req)
-        if(err.isEmpty()){
-
-            const usuario = await User.findOne({email: req.body.email})
-        
-            if(usuario==null){  //valida si el usuario existe en la base de datos en base a la busqueda anterior atraves del email.
-                res.status(201).res.json({msg:"Password or email incorrect"}) //agregar codigo de estado
-            }    
-            if(!bcrypt.compareSync(req.body.password, usuario.password)){ //compara con la contraseña guardada en la  base dedatos
-                res.status(201).res.json({msg:"Password or email incorrect"})
-            }
-
-            const user = {
-                _id: usuario._id,
-                name: usuario.name
-            }
-            req.session.user = user
-
-            //checkbox           //si el usuario precisa mantener la sesion activa, guarda en cookie la sesion del usuario, para que al cerrar no se cierre
-            if(req.body.remember){ //esto es por si se tiene un checkbox seleccionado como true para guardar en sesión usuario y contraseña. es decir, en cookie  
-                res.cookie("sessionDelUsuario", req.session.user, {maxAge:60000*60*24})
-            }
-            res.json({msg:"User Logged"})
-        } else {
-            res.status(501).json(err)
-        }
-    } catch(error) {
-        res.status(501).json({error})
-    }
-}
-/*const loginToken = async (req, res) =>{
-    try {
-        const err = validationResult(req)
-        if(err.isEmpty()){
-
-            const usuario = await User.findOne({email: req.body.email})
-            res.status(201).json({msg:"product updated"})
-        
-            if(usuario==null){  //valida si el usuario existe en la base de datos.
-                res.json({msg:"Contraseña o Email Incorrectos"}) //agregar codigo de estado
-            }    
-            
-            if(bcrypt.compareSync(req.body.password, usuario.password)){ //compara con la contraseña guardada en la  base dedatos
-                res.json({msg: "Contraseña o Email Incorrectos"})
-            }
-
-            const token = await generateToken({id:usuario_id,email:_email})
-                _id: usuario._id,
-                name: usuario.name
-            }
-            req.session.user = user
-
-            //checkbox           //si el usuario precisa mantener la sesion activa, guarda en cookie la sesion del usuario, para que al cerrar no se cierre
-            if(req.body.remember){ //esto es por si se tiene un checkbox  
-                res.cookie("sessionDelUsuario",req.session.user, {maxAge:60000*60*24})
-            }
-            res.json({email: req.body.email,token})
-        } else {
-            res.status(501).json(err)
-        }
-    } catch(error) {
-        res.json({error})
-    }
-}*/
 
 
 
@@ -133,12 +63,6 @@ const verSession = async (req,res) =>{
 const verCookie = async (req,res) =>{
     res.json(req.cookies.ItemInSession)
 }
-//jwt
-/*const sendToken = async (req,res) =>{
-    const token = await generateToken(req.body)
-}*/
-
-
 
 
 //put U-pdate
@@ -180,20 +104,10 @@ const eliminarItem = async(req, res) => {
 const eliminarCookie = async(req, res) => {
     res.clearCookie("ItemInSession") //cuidado que arriba esta con otro nombre
     res.json({msg:'cookie deleted'})
-}/*
-const logOut = (req, res) => {
-    res.clearCookie("sessionDelUsuario") //cuidado que tiene otro nombre arriba
-    req.session.destroy()
-    res.json({msg:"Session Closed"}) 
-}*/
-const deleteAll = async(req, res) => {
-//const colection = mongoose.Collection.collectionName.find(req.params.collectionName)
-const result = await Student.deleteMany({});
-    res.status(200).json(`Deleted + ${result.deletedCount} + documents`)
 }
 
 
 
 
-module.exports = {/*sendToken,logOut,*/loginUsuario,eliminarCookie,verCookie,verSession,crearSession,vistaGeneral,crearItem,vistaUnitaria,busquedaUnitaria, editarItem, eliminarItem,cerrarSession,deleteAll}
+module.exports = {eliminarCookie,verCookie,verSession,crearSession,vistaGeneral,crearItem,vistaUnitaria,busquedaUnitaria, editarItem, eliminarItem,cerrarSession}
  
