efaf7ba60df95e8d61d84f1358344cf5f793c631
41dr3w <aavlqz@gmail.com>
2022-12-02 01:11:08 -0300

    setting for vercel again

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const app = require("./app")
 require("dotenv").config()
 const port = process.env.PORT || 3000
 
+ // Create GET request
+ app.get("/", (req, res) => {
+  res.send("Server's running and DB connect it, go to the link to see the documentation and use the api server");
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   }) 
 
- // Create GET request
-app.get("/", (req, res) => {
-  res.send("Server's running and DB connect it, go to the link to see the documentation and use the api server");
-});
